6dd24ccee72a8bcdbf5857b250fb10b97f14d115
taolee <taolee.cn@gmail.com>
2013-03-11 14:59:52 +0000

    firstly clone the remote, then local commit, then push to remote

---
+++ test.py
@@ -0,0 +1,29 @@
+﻿for i in range(1,5):
+    print 'Hello World!'
+
+n = 0
+while n<=10:
+    print n
+    n=n+1
+
+for n in range(1,10+1):
+    print n
+
+a='I '
+b='Love '
+c='You!'
+print a,b,c
+
+import random
+for n in range(1,6+1):
+    print(random.randint(1,100))
+
+while n!=13:
+    n=random.randint(1,100)
+    print n
+
+mytext=["I","Love","My","Raspberry","Pi"]
+for n in range(0,5):
+    print(mytext[n])
+print 
+
